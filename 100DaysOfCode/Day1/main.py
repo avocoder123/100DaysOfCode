e65381048b3f328c3d48523a50c82a66a9deb5f5
aguacoder123 <52142361+aguacoder123@users.noreply.github.com>
2021-01-29 23:46:32 -0500

    Day 2

---
+++ 100DaysOfCode/Day1/main.py
@@ -0,0 +1,51 @@
+#Write your code below this line 👇
+
+
+print("Hello World")
+print("Day 1 - Python Print Function")
+print("This function is declared like this:")
+print("print('what to print')")
+
+print("Hello world \n Hello world")
+
+print("This" + "is" + "how" + "you" + "concatenate")
+
+#Fix the code below
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print("e.g. print('Hello ' + 'world')")
+print("New lines can be created with a backslash and n.")
+
+print(len(input("What is your name?")))
+
+#variable
+name = input("what is your name?")
+print(name)
+length = len(name)
+print(length)
+
+#challenge
+# Don't change the code below
+a = input("a: ")
+b = input("b: ")
+#  Don't change the code above
+
+####################################
+#Write your code below this line
+
+c = a
+a = b
+b = c
+
+
+#Write your code above this line
+####################################
+
+#  Don't change the code below
+print("a: " + a)
+print("b: " + b)
+
+
+
+
